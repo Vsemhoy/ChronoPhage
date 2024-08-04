6d3745ee416cde953b0bd8091783be943b6e8133
Max Matt <77360455+Vsemhoy@users.noreply.github.com>
2024-08-04 22:36:58 +0300

    Start empty project

---
+++ ChronoPhage/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ChronoPhage
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ChronoPhage/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ChronoPhage
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
